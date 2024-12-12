43c8dbfd8af8e509c24552d65aecab78f9cdaca5
Kuwan <phiex117@gmail.com>
2024-12-12 09:37:56 +0400

    lang v1

--- main.js
+++ main.js
@@ -14,3 +14,15 @@ window.onclick = function(event) {
         closeModal();
     }
 }
+
+function toggleDropdown(element) {
+    element.classList.toggle('active');
+}
+
+// Закрытие меню при клике вне его
+document.addEventListener('click', function (event) {
+    const selector = document.querySelector('.language-selector');
+    if (!selector.contains(event.target)) {
+        selector.classList.remove('active');
+    }
+});
